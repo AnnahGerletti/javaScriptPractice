90875d62069c8ae2e4af7579d61bc2bc0784941e
AnnahGerletti <agerletti@gmail.com>
2018-03-10 13:48:22 +1300

    algorithm works, used new inputs

--- practice2.js
+++ practice2.js
@@ -6,28 +6,29 @@ var myArray = Array(100)
     return index + 1
   })
 
-myArray.map(num => {
-  var changeNum = (num * 2) + 5
+var results = myArray.map(num => {
+  var changeNum = (num * 3) + 3
   if (changeNum < 100) {
     if (changeNum % 2 === 0) {
-      console.log(changeNum + ' is a SMALL EVEN number')
+      return (changeNum + ' is a SMALL EVEN number')
     } else {
-      console.log(changeNum + ' is a SMALL ODD number')
+      return (changeNum + ' is a SMALL ODD number')
     }
   } else if (changeNum > 100 && changeNum < 150) {
     if (changeNum % 4 === 0) {
-      console.log(changeNum + ' is BIG and / by 4')
+      return (changeNum + ' is BIG and / by 4')
     } else {
-      console.log(changeNum + ' is BIG but NOT / by 4')
+      return (changeNum + ' is BIG but NOT / by 4')
     }
   } else if (changeNum > 150) {
     if (changeNum % 2 !== 0) {
-      console.log(changeNum + ' is BIG and ODD')
+      return (changeNum + ' is BIG and ODD')
     } else {
-      console.log(changeNum + 'is BIG but EVEN')
+      return (changeNum + ' is BIG but EVEN')
     }
-  }
-  else {
-    console.log(changeNum)
+  } else {
+    return (changeNum)
   }
 })
+
+console.log(results)
