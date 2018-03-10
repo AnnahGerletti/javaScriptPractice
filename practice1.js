992954d7bc4b9a7d15a096c9d00200b1c4809684
AnnahGerletti <agerletti@gmail.com>
2018-03-10 13:36:50 +1300

    using a .map have to return out new arr

--- practice1.js
+++ practice1.js
@@ -132,15 +132,15 @@ console.log(foo)
 // Line Break
 // Uppercase every 2nd letter of the str
 
-// var magicWords = 'programming is magical and awesome'
-// var replaceStr = magicWords.replace(/\s/g, '')
-// var newArr = replaceStr.split('')
-// newArr.map((arr, index) => {
-//   var finalStr = ''
-//   if (index % 2 === 0) {
-//     finalStr += arr.toString().toUpperCase()
-//   } else {
-//     return finalStr
-//   }
-//   console.log(finalStr)
-// })
+var magicWords = 'programming is magical and awesome'
+var replaceStr = magicWords.replace(/\s/g, '')
+var newArr = replaceStr.split('')
+var finalArr = newArr.map((arr, index) => {
+  if ((index + 1) % 2 === 0) {
+    return arr.toString().toUpperCase()
+  } else {
+    return arr
+  }
+})
+var finalStr = finalArr.join('')
+console.log(finalStr)
