7940732963232fa9815ef1e947519156c4d09bbd
AnnahGerletti <agerletti@gmail.com>
2018-03-12 12:05:18 +1300

    practice working with idexes

--- practice2.js
+++ practice2.js
@@ -76,7 +76,7 @@ console.log(refactorResults)
 
 // LineBreak
 // Multiply numbers output the results
-console.log('\n' + "numbers")
+console.log('\n' + 'numbers')
 
 var oneArr = [1, 3, 5, 6, 8, 9, 14]
 var twoArr = [9, 33, 52, 2, 1, 5, 88]
@@ -84,4 +84,28 @@ var twoArr = [9, 33, 52, 2, 1, 5, 88]
 oneArr.forEach((input, index) => {
   var results = oneArr[index] * twoArr[index]
   console.log(results)
+})
+
+console.log('\n')
+// LineBreak
+// Index practices
+
+// Write out the number from each array at position 5.
+// Write out every second number from each array
+// Merge both arrays together and print out every third number
+
+var oneArray = [1, 3, 5, 6, 8, 9, 14]
+var twoArray = [ 9, 33, 52, 2, 1, 5, 88]
+
+var result = oneArray[5]
+var result2 = twoArray[5]
+console.log(result)
+console.log(result2)
+
+console.log('every second Num' + '\n')
+
+oneArray.forEach((input, index) => {
+  if (index % 2 !== 0) {
+    console.log('1st Array ' + input) 
+  }
 })
